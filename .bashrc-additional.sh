92c6d1b14e9c2199f79408553eb462ce69e5997b
Cyb10101 <cyb10101@gmail.com>
2019-06-04 22:43:12 +0200

    Style bash improved

--- .bashrc-additional.sh
+++ .bashrc-additional.sh
@@ -1,9 +1,9 @@
 source ~/.shell-methods.sh
 sshAgentRestart
 sshAgentAddKey 7d ~/.ssh/id_rsa
-stylePS1
 bashCompletion
 addAlias
 addDockerAlias
+stylePS1 "${DOCKER_COMPOSE_PROJECT}"
 
 export PATH=${PATH}:~/.composer/vendor/bin:./bin:./vendor/bin:./node_modules/.bin
--- .shell-methods.sh
+++ .shell-methods.sh
@@ -1,5 +1,23 @@
 #!/usr/bin/env bash
 
+COLOR_RESET=$(echo -en '\001\033[0m\002')
+COLOR_RED=$(echo -en '\001\033[00;31m\002')
+COLOR_GREEN=$(echo -en '\001\033[00;32m\002')
+COLOR_YELLOW=$(echo -en '\001\033[00;33m\002')
+COLOR_BLUE=$(echo -en '\001\033[00;34m\002')
+COLOR_MAGENTA=$(echo -en '\001\033[00;35m\002')
+COLOR_PURPLE=$(echo -en '\001\033[00;35m\002')
+COLOR_CYAN=$(echo -en '\001\033[00;36m\002')
+COLOR_LIGHTGRAY=$(echo -en '\001\033[00;37m\002')
+COLOR_LRED=$(echo -en '\001\033[01;31m\002')
+COLOR_LGREEN=$(echo -en '\001\033[01;32m\002')
+COLOR_LYELLOW=$(echo -en '\001\033[01;33m\002')
+COLOR_LBLUE=$(echo -en '\001\033[01;34m\002')
+COLOR_LMAGENTA=$(echo -en '\001\033[01;35m\002')
+COLOR_LPURPLE=$(echo -en '\001\033[01;35m\002')
+COLOR_LCYAN=$(echo -en '\001\033[01;36m\002')
+COLOR_WHITE=$(echo -en '\001\033[01;37m\002')
+
 # Set Terminal title
 function userTerminalTitle {
     echo -e '\033]2;'$1'\007'
@@ -13,9 +31,16 @@ function userTerminalTitlePwd {
 # Set current user color
 function userColorUser {
     if [[ $EUID -eq 0 ]]; then
-        echo -e '\e[1;31m';
+        echo "${COLOR_LRED}";
     else
-        echo -e '\e[1;32m';
+        echo "${COLOR_LGREEN}";
+    fi
+}
+
+# Render Git branch for PS1
+function renderGitBranch {
+    if [ -f $(which git) ]; then
+        echo "${COLOR_YELLOW}$(__git_ps1)"
     fi
 }
 
@@ -48,10 +73,16 @@ function sshAgentAddKeyOld {
 
 # Style bash prompt
 function stylePS1 {
-    PS1='$(userTerminalTitlePwd)\[\e[0;36m\][$(userColorUser)\u\[\e[0;36m\]@\[\e[1;34m\]$DOCKER_COMPOSE_PROJECT\[\e[0;36m\]: \[\e[0m\]\w\[\e[0;36m\]]\[\e[0;36m\]> $(userColorUser)\n\$\[\e[0m\] ';
-    if [ -f $(which git) ]; then
-        PS1='$(userTerminalTitlePwd)\[\e[0;36m\][$(userColorUser)\u\[\e[0;36m\]@\[\e[1;34m\]$DOCKER_COMPOSE_PROJECT\[\e[0;36m\]: \[\e[0m\]\w\[\e[0;36m\]]\[\e[0;33m\]$(__git_ps1)\[\e[0;36m\]> $(userColorUser)\n\$\[\e[0m\] ';
-    fi
+    VAR_HOSTNAME=`hostname`
+    if [ ! -z "$1" ]; then
+        VAR_HOSTNAME="${1}"
+    fi;
+
+    PS1='$(userTerminalTitlePwd)'
+    PS1+='${COLOR_CYAN}['
+    PS1+='$(userColorUser)\u${COLOR_CYAN}@${COLOR_LBLUE}${VAR_HOSTNAME}${COLOR_CYAN}: ${COLOR_RESET}\w'
+    PS1+='${COLOR_CYAN}]'
+    PS1+='$(renderGitBranch)${COLOR_CYAN}> $(userColorUser)\n\$${COLOR_RESET} ';
 }
 
 # Style bash prompt
